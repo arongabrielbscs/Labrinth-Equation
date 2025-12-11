91e46e6411052b4069eb56ac628cc31fc5b6c060
arongabrielbscs <arongabrielbscs@users.noreply.github.com>
2025-12-11 16:10:39 +0000

    Merge pull request #12 from arongabrielbscs/aron/new-collision
    Aron/new collision [Automated Deployment] 9e5d92697246f07cb5da211b90b1fd220eb1b26b

--- html/html.nocache.js
+++ html/html.nocache.js
@@ -361,8 +361,8 @@ function html(){
     }
     var strongName;
     try {
-      unflattenKeylistIntoAnswers(['gecko1_8'], 'DDE1FB916258612B197FEAA748775424');
-      unflattenKeylistIntoAnswers(['safari'], 'DDE1FB916258612B197FEAA748775424' + ':1');
+      unflattenKeylistIntoAnswers(['gecko1_8'], '6665E239640762B34F2D09FD17B3694D');
+      unflattenKeylistIntoAnswers(['safari'], '6665E239640762B34F2D09FD17B3694D' + ':1');
       strongName = answers[computePropValue('user.agent')];
       var idx = strongName.indexOf(':');
       if (idx != -1) {
