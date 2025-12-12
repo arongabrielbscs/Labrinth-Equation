262136e3ef6228bd4171a0c6665c01ce159a2f68
Aronic <aronic.tekkuniku@example.com>
2025-12-12 14:05:38 +0800

    Change the Boss Order and their parameters

--- core/src/main/java/gg/group3/justgo/GameLevel.java
+++ core/src/main/java/gg/group3/justgo/GameLevel.java
@@ -25,9 +25,9 @@ public class GameLevel {
         Beanite(2, 1, 2, 2),
         RatFly(0, 1, 3, 2),
         RatGhoul(1, 1, 5, 3),
-        GhoulKing(12, 0, 64, 10, 2),
-        WillowQueen(12, 4, 64, 15, 4),
-        Vessel(12, 8, 64, 20, 6)
+        WillowQueen(12, 4, 64, 10, 2),
+        Vessel(12, 8, 64, 15, 4),
+        GhoulKing(12, 0, 64, 20, 6)
         ;
 
         // Fields to store the data
